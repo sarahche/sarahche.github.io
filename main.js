78228cfe765c97141368b55343d37dbeea547e96
sarahche <sarahche@andrew.cmu.edu>
2021-04-08 16:23:26 -0400

    Initial 6B commit

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+
+function onload() {
+	let shoppingCartCount = sessionStorage.getItem("shoppingCartCount");
+	let selectedProductinCart = null;
+    shoppingCartCount = parseInt(shoppingCartCount);
+	if (shoppingCartCount) {
+		sessionStorage.setItem("shoppingCartCount", JSON.stringify(shoppingCartCount));
+	} else {
+		let shoppingCartCount = 0;
+		sessionStorage.setItem("shoppingCartCount", 0);
+		console.log("elsesecond", shoppingCartCount);}
+	document.getElementById("shoppingCartCount").textContent = shoppingCartCount;
+}
+
 // To updated images from dropdown options 
 var imageList = [
   "assets/catharnessstrawberry.png",
@@ -6,12 +20,37 @@ var imageList = [
   "assets/catharnessorange.jpeg"
    ]
 
+// Sets an array of products
+ let products = [
+	{name: 'Strawberry',
+	tag: 'strawberry',
+	price: 20,
+	inCart: 0},
+	{name: 'Blackberry',
+	tag: 'blackberry',
+	price: 20,
+	inCart: 0},
+	{name: 'Crazyberry',
+	tag: 'crazyberry',
+	price: 20,
+	inCart: 0},
+	{name: 'FireOrange',
+	tag: 'fireorange',
+	price: 20,
+	inCart: 0}
+	]
+
+var currentColor = "strawberry"
+
+
+// Changes main product image
 function switchImage() {
     document.mainProductImg.src = imageList[document.colorDropdown.color.options[document.colorDropdown.color.selectedIndex].value];
+    var chosenColor = products[document.colorDropdown.color.options[document.colorDropdown.color.selectedIndex].value].tag;
+    currentColor = chosenColor;
 }
 
 // To allow user to change number of items 
-
 var itemCount = 1
 function onClickPlus() {
   itemCount += 1;
@@ -24,12 +63,43 @@ function onClickMinus() {
     document.getElementById("itemCount").textContent = itemCount;}
 }
 
-// Reflects number of items in cart 
-
-var shoppingCartCount = 0
-
+// Reflects number of items in cart
 function onClickAddCart() {
-  localStorage.setItem("shoppingCartCount", JSON.stringify(shoppingCartCount));
+  let shoppingCartCount = sessionStorage.getItem("shoppingCartCount");
+  shoppingCartCount = parseInt(shoppingCartCount);
   shoppingCartCount += itemCount; 
-  document.getElementById("shoppingCartCount").textContent = shoppingCartCount;
+  if (shoppingCartCount) {
+		sessionStorage.setItem("shoppingCartCount", JSON.stringify(shoppingCartCount));
+	} else {
+		sessionStorage.setItem("shoppingCartCount", 0);
+		console.log("elsesecond", shoppingCartCount);}
+  document.getElementById("shoppingCartCount").innerHTML = shoppingCartCount;
+  setItems();
 } 
+
+
+function setItems() {
+	selectedProduct = products[document.colorDropdown.color.options[document.colorDropdown.color.selectedIndex].value]
+	let cartItems = sessionStorage.getItem("allCartProducts");
+	cartItems = JSON.parse(cartItems);
+	if (cartItems != null) {
+		if (cartItems[selectedProduct.tag] == undefined) {
+			cartItems = {
+				...cartItems,
+				[selectedProduct.tag]: selectedProduct
+			}
+		}
+		cartItems[selectedProduct.tag].inCart += itemCount;
+	} 
+	else {
+		selectedProduct.inCart = 1;
+		cartItems = {
+		[selectedProduct.tag]: selectedProduct
+	};
+	}
+	console.log(cartItems);
+	//let allCartProducts = JSON.stringify(cartItems);
+	sessionStorage.setItem("allCartProducts", JSON.stringify(cartItems));
+	console.log(allCartProducts);
+
+}
